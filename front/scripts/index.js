e632258c8e87d98f98632fc4da14c0a861f9238c
daramirezsu06 <daramirezsu06@gmail.com>
2024-02-11 20:37:44 -0500

    se agregaron los estilos con boobtrap pero no quedo resposive

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -10,56 +10,37 @@ function addMovie({
   poster,
 }) {
   const moviecart = document.createElement("div");
-  const contentonecart = document.createElement("div");
-  const infomovie = document.createElement("div");
-  const mtitle = document.createElement("h4");
-  const myear = document.createElement("p");
-  const mimagenpeli = document.createElement("img");
-  const mdirector = document.createElement("p");
-  const mduration = document.createElement("p");
-  const mgenre = document.createElement("p");
-  const mrate = document.createElement("p");
+  moviecart.classList.add("col-md-4"); // Agrega las clases de Bootstrap para el diseño responsivo
 
-  mtitle.textContent = title;
-  myear.textContent = `Año: ${year}`;
-  mdirector.innerHTML = `Director ${director}`;
-  mimagenpeli.src = poster;
-  mduration.textContent = `Duracion: ${duration}`;
-  mgenre.textContent = `Genero: ${genre.join(", ")}`;
-  mrate.textContent = `valoracion: ${rate} :star:`;
-  moviecart.className = "moviescarts";
-  infomovie.className = "infocontent";
-  moviecart.classList.add();
-  moviecart.classList.add("card");
-  mimagenpeli.classList.add("card-img-top");
-  infomovie.classList.add("card-body");
-  mtitle.classList.add("card-title");
-  contentonecart.classList.add("col-sm");
+  moviecart.innerHTML = `
+    <div class="cardmovie">
+    <div class="card bg-white p-4 border  ">
+      <img class="imgmovie card-img-top mx-auto" src="${poster}" alt="Card image cap">
+      <div class="cardinfo d-flex flex-column justify-content-between">
+        <h5>${title}</h5>
+        <p>Año ${year} | Director: ${director} | ${duration} | ${rate} (Puntuación de la película) | ${genre.join(
+    ", "
+  )} </p>
+        <p class="card-text"><small class="text-muted">Todo público</small></p>
+      </div>
+    </div>
+      
+    </div>
+    `;
 
-  infomovie.appendChild(mtitle);
-  infomovie.appendChild(myear);
-  infomovie.appendChild(mdirector);
-  infomovie.appendChild(mduration);
-  infomovie.appendChild(mgenre);
-  infomovie.appendChild(mrate);
-
-  
-  moviecart.appendChild(mimagenpeli);
-  moviecart.appendChild(infomovie);
-  contentonecart.appendChild(moviecart);
-
-  return contentonecart;
+  return moviecart;
 }
-
-/*const arraymovies = tempData.map((pelicula) => addMovie(pelicula));*/
 
 function renderMovies() {
   $.get("https://students-api.2.us-1.fl0.io/movies", (movies, state) => {
-    const arraymovies = movies.map((pelicula) => addMovie(pelicula));
-    arraymovies.forEach((element) => {
-      principalContent.appendChild(element);
+    const cardDeck = document.createElement("div");
+
+
+    movies.forEach((pelicula) => {
+      const moviecart = addMovie(pelicula);
+      principalContent.appendChild(moviecart);
     });
-    console.log(state);
+
     console.log(state);
   });
 }
